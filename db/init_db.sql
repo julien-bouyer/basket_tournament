1613bba414dea0e5bcd49a8a4bc844e96f5ccc85
Julien <julienbouyer@gmail.com>
2018-10-17 22:55:03 +0200

    init db and data

--- db/init_db.sql
+++ db/init_db.sql
@@ -2,118 +2,108 @@ drop database if exists basket_tournament;
 create database basket_tournament;
 use basket_tournament;
 
-drop table if exists tournoi;
-create table tournoi (
-    id smallint unsigned not null auto_increment,
-    nom varchar(30) not null,
-    date_tournoi datetime not null,
-    saison varchar(15), -- 2018-2019
-    lieu varchar(30),
+drop table if exists tournament;
+create table tournament (
+    id smallint not null auto_increment,
+    name varchar(30) not null,
+    date_tournament datetime not null,
+    season varchar(15), -- 2018-2019
+    place varchar(30),
     primary key (id)
-)
-engine=InnoDB default charset=latin1;
+);
 
-drop table if exists equipe;
-create table equipe (
-    id smallint unsigned not null auto_increment,
-    nom varchar(20) not null,
-    commune varchar(40) not null,
-    sexe varchar(8), -- MASCULIN / FEMININ
-    departement int,
+drop table if exists team;
+create table team (
+    id smallint not null auto_increment,
+    name varchar(40) not null,
+    city varchar(40) not null,
+    sex varchar(8), -- MASCULIN / FEMININ
+    county int,
     primary key (id)
-)
-engine=InnoDB default charset=latin1;
+);
 
-drop table if exists joueur;
-create table joueur (
-    id int unsigned not null auto_increment,
-    nom varchar(40) not null,
-    prenom varchar(40) not null,
-    date_naissance datetime,
-    sexe varchar(5), -- HOMME / FEMME
-    numero smallint,
+drop table if exists player;
+create table player (
+    id smallint not null auto_increment,
+    lastname varchar(40) not null,
+    firstname varchar(40) not null,
+    birthday datetime,
+    sex varchar(5), -- HOMME / FEMME
+    num smallint,
     primary key (id)
-)
-engine=InnoDB default charset=latin1;
+);
 
 drop table if exists phase;
 create table phase (
-    id int unsigned not null auto_increment,
-    nom varchar(20),
+    id smallint not null auto_increment,
+    name varchar(20),
     type varchar(20) not null, -- POULE / PHASE_FINALE / FINALE / ...
-    terminee tinyint,
-    sexe varchar(5), -- HOMME / FEMME
-    id_tournoi smallint,
+    completed tinyint,
+    sex varchar(5), -- HOMME / FEMME
+    id_tournament smallint,
     primary key (id),
-    constraint fk_phase_tournoi foreign key (id_tournoi) references tournoi(id)
-)
-engine=InnoDB default charset=latin1;
+    constraint fk_phase_tournament foreign key (id_tournament) references tournament(id)
+);
 
-drop table if exists match;
-create table match (
-    id int unsigned not null auto_increment,
+drop table if exists game;
+create table game (
+    id smallint not null auto_increment,
     terrain varchar(2) not null, -- A, B, C...
     horaire time not null,
     termine tinyint,
-    id_equipe_gagante smallint,
-    id_phase int,
+    id_winningteam smallint,
+    id_phase smallint,
     primary key (id),
-    constraint fk_match_equipe_gagnante foreign key (id_equipe_gagante) references equipe(id),
-    constraint fk_match_phase foreign key (id_phase) references phase(id)
-)
-engine=InnoDB default charset=latin1;
+    constraint fk_game_winningteam foreign key (id_winningteam) references team(id),
+    constraint fk_game_phase foreign key (id_phase) references phase(id)
+);
 
-drop table if exists tournoi_equipe;
-create table tournoi_equipe (
-    id_tournoi smallint not null,
-    id_equipe smallint not null,
-    primary key (id_tournoi, id_equipe),
-    constraint fk_tournoi_equipe_tournoi foreign key (id_tournoi) references tournoi(id),
-    constraint fk_tournoi_equipe_equipe foreign key (id_equipe) references equipe(id)
-)
-engine=InnoDB default charset=latin1;
+drop table if exists tournament_team;
+create table tournament_team (
+    id_tournament smallint not null,
+    id_team smallint not null,
+    primary key (id_tournament, id_team),
+    constraint fk_tournament_team_tournament foreign key (id_tournament) references tournament(id),
+    constraint fk_tournament_team_team foreign key (id_team) references team(id)
+);
 
-drop table if exists equipe_joueur;
-create table equipe_joueur (
-    id_equipe smallint not null,
-    id_joueur int not null,
-    primary key (id_equipe, id_joueur),
-    constraint fk_equipe_joueur_equipe foreign key (id_equipe) references equipe(id),
-    constraint fk_equipe_joueur_joueur foreign key (id_joueur) references joueur(id)
-)
-engine=InnoDB default charset=latin1;
+drop table if exists team_player;
+create table team_player (
+    id_team smallint not null,
+    id_player smallint not null,
+    primary key (id_team, id_player),
+    constraint fk_team_player_team foreign key (id_team) references team(id),
+    constraint fk_team_player_player foreign key (id_player) references player(id)
+);
 
-drop table if exists equipe_phase;
-create table equipe_phase (
-    id_equipe smallint not null,
-    id_phase int not null,
+drop table if exists team_phase;
+create table team_phase (
+    id_team smallint not null,
+    id_phase smallint not null,
     points bigint,
-    marques bigint,
-    encaisses bigint,
-    primary key (id_equipe, id_phase),
-    constraint fk_equipe_phase_equipe foreign key (id_equipe) references equipe(id),
-    constraint fk_equipe_phase_phase foreign key (id_phase) references phase(id)
-)
-engine=InnoDB default charset=latin1;
+    pointsFor bigint,
+    pointsAgainst bigint,
+    primary key (id_team, id_phase),
+    constraint fk_team_phase_team foreign key (id_team) references team(id),
+    constraint fk_team_phase_phase foreign key (id_phase) references phase(id)
+);
 
-drop table if exists equipe_match;
-create table equipe_match (
-    id_equipe smallint not null,
-    id_match int not null,
+drop table if exists team_game;
+create table team_game (
+    id_team smallint not null,
+    id_game smallint not null,
     points bigint,
-    primary key (id_equipe, id_match),
-    constraint fk_equipe_match_equipe foreign key (id_equipe) references equipe(id),
-    constraint fk_equipe_match_match foreign key (id_match) references match(id)
-)
-engine=InnoDB default charset=latin1;
+    primary key (id_team, id_game),
+    constraint fk_team_game_team foreign key (id_team) references team(id),
+    constraint fk_team_game_game foreign key (id_game) references game(id)
+);
 
-drop table if exists joueur_match;
-create table joueur_match (
-    id_joueur smallint not null,
-    id_match int not null,
+drop table if exists player_game;
+create table player_game (
+    id_player smallint not null,
+    id_game smallint not null,
     points bigint,
-    primary key (id_joueur, id_match),
-    constraint fk_joueur_match_joueur foreign key (id_joueur) references joueur(id),
-    constraint fk_joueur_match_match foreign key (id_match) references match(id)
-)
-engine=InnoDB default charset=latin1;
+    primary key (id_player, id_game),
+    constraint fk_player_game_player foreign key (id_player) references player(id),
+    constraint fk_player_game_game foreign key (id_game) references game(id)
+);
