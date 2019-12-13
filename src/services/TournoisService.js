35b07633bcda4735c48a40769869912a57ca0113
Julien BOUYER <julienbouyer@gmail.com>
2019-12-13 23:04:13 +0100

    Tournois : view and service

--- src/services/TournoisService.js
+++ src/services/TournoisService.js
@@ -1,1 +1,31 @@
-export default {}
+import axios from 'axios';
+
+const BACKEND_URL = process.env.TOURNOIS_BACKEND_URL;
+
+export default {
+    fetch() {
+        return axios.get(BACKEND_URL)
+            .then(response => response.data)
+            .catch(error => { throw error; });
+    },
+    delete(id) {
+        return axios.delete(`${BACKEND_URL}/${id}`)
+            .then(response => response.data)
+            .catch(error => { throw error; });
+    },
+    create(tournoi) {
+        return axios.post(BACKEND_URL, tournoi)
+            .then(response => response.data)
+            .catch(error => { throw error; });
+    },
+    fetchOne(id) {
+        return axios.get(`${BACKEND_URL}/${id}`)
+            .then(response => response.data)
+            .catch(error => { throw error; });
+    },
+    update(tournoi) {
+        return axios.put(`${BACKEND_URL}/${tournoi.id}`, tournoi)
+            .then(response => response.data)
+            .catch(error => { throw error; });
+    }
+}
--- src/views/Tournois.vue
+++ src/views/Tournois.vue
@@ -1,11 +1,48 @@
 <template>
     <section class="container">
-        tournois
+        <md-table v-once>
+            <md-table-header>
+                <md-table-row>
+                    <md-table-head>Nom</md-table-head>
+                    <md-table-head>Date</md-table-head>
+                    <md-table-head>Saison</md-table-head>
+                    <md-table-head>Lieu</md-table-head>
+                    <md-table-head>Actions</md-table-head>
+                </md-table-row>
+            </md-table-header>
+            <md-table-body>
+                <md-table-row v-for="(tournoi, index) in tournois" :key="index">
+                    <md-table-cell>{{tournoi.name}}</md-table-cell>
+                    <md-table-cell>{{tournoi.date_tournament}}</md-table-cell>
+                    <md-table-cell>{{tournoi.season}}</md-table-cell>
+                    <md-table-cell>{{tournoi.place}}</md-table-cell>
+                    <md-table-cell>
+                        <md-button @click="edit(tournoi.id)">
+                            <md-icon class="md-size-2x">edit</md-icon>
+                        </md-button>
+                        <md-button @click="start(tournoi.id)">
+                            <md-icon class="md-size-2x">play_arrow</md-icon>
+                        </md-button>
+                        <md-button @click="stop(tournoi.id)">
+                            <md-icon class="md-size-2x">stop</md-icon>
+                        </md-button>
+                        <md-button @click="remove(tournoi.id)">
+                            <md-icon class="md-size-2x">delete</md-icon>
+                        </md-button>
+                    </md-table-cell>
+                </md-table-row>
+            </md-table-body>
+        </md-table>
     </section>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                tournois: []
+            }
+        }
     };
 </script>
 
