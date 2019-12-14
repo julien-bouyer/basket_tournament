a583c92bb7986faec472adad8d8667e6747d6064
Julien BOUYER <julienbouyer@gmail.com>
2019-12-14 16:09:37 +0100

    Add Vuetify, remove VueMaterial

--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
 import Vue from 'vue';
-import VueMaterial from 'vue-material';
+import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router';
-import 'vue-material/dist/vue-material.css';
 import App from './App';
 import router from './views/router.js';
 import store from './store';
 
 Vue.use(VueRouter);
-Vue.use(VueMaterial);
-
-Vue.material.registerTheme('orange', {
-    primary: 'orange',
-    accent: 'grey',
-    warn: 'red'
-});
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
     store,
+    vuetify,
     render: h => h(App),
 });
