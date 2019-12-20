1200cccba79b65fbc9a3b9db81f979a98e2128f1
Julien BOUYER <julienbouyer@gmail.com>
2019-12-21 00:24:09 +0100

    Add confirm dialog component

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-dialog v-model="display" max-width="290">
+        <v-card>
+            <v-card-title class="headline">{{title}}</v-card-title>
+            <v-card-text>{{content}}</v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="$emit('close')">Non</v-btn>
+                <v-btn color="green darken-1" text @click="$emit('confirm')">Oui</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                display: false,
+            }
+        },
+        props: [
+            'confirmDialog',
+            'title',
+            'content'
+        ],
+        watch: {
+            confirmDialog: function (newVal) {
+                this.display = newVal;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
