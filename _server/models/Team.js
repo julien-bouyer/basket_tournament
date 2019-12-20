7d417de8c8bd468b10be288f7dad11278f722101
Julien BOUYER <julienbouyer@gmail.com>
2019-12-21 00:23:42 +0100

    Server code improvements

--- _server/models/Team.js
+++ _server/models/Team.js
@@ -1,23 +1,23 @@
 const db = require('../db/database'); //reference of db/database.js
 const Team = {
-    getAll: function (callback) {
+    getAll(callback) {
         return db.query('select * from team', callback);
     },
-    getById: function (id, callback) {
+    getById(id, callback) {
         return db.query('select * from team where id = ?', [id], callback);
     },
-    add: function (team, callback) {
+    add(team, callback) {
         return db.query('insert into team (name, city, sex, county) values (?, ?, ?, ?)', [team.name, team.city, team.sex, team.county], callback);
     },
-    delete: function (id, callback) {
+    delete(id, callback) {
         return db.query('delete from team where id = ?', [id], callback);
     },
-    update: function (id, team, callback) {
+    update(id, team, callback) {
         return db.query('update team set name = ?, city = ?, sex = ?, county = ? where id = ?', [team.name, team.city, team.sex, team.county, id], callback);
     },
-    deleteAll: function (items, callback) {
+    deleteAll(items, callback) {
         let delarr = [];
-        [].forEach.call(items, function (item) {
+        [].forEach.call(items, item => {
             delarr.push(item);
         });
         return db.query('delete from team where id in (?)', [delarr], callback);
--- _server/models/Tournament.js
+++ _server/models/Tournament.js
@@ -1,23 +1,23 @@
 const db = require('../db/database'); //reference of db/database.js
 const Tournament = {
-    getAll: function (callback) {
+    getAll(callback) {
         return db.query('select * from tournament', callback);
     },
-    getById: function (id, callback) {
+    getById(id, callback) {
         return db.query('select * from tournament where id = ?', [id], callback);
     },
-    add: function (tournament, callback) {
+    add(tournament, callback) {
         return db.query('insert into tournament (name, date_tournament, season, place) values (?, ?, ?, ?)', [tournament.name, tournament.date_tournament, tournament.season, tournament.place], callback);
     },
-    delete: function (id, callback) {
+    delete(id, callback) {
         return db.query('delete from tournament where id = ?', [id], callback);
     },
-    update: function (id, tournament, callback) {
+    update(id, tournament, callback) {
         return db.query('update tournament set name = ?, date_tournament = ?, season = ?, place = ? where id = ?', [tournament.name, tournament.date_tournament, tournament.season, tournament.place, id], callback);
     },
-    deleteAll: function (items, callback) {
+    deleteAll(items, callback) {
         let delarr = [];
-        [].forEach.call(items, function (item) {
+        [].forEach.call(items, item => {
             delarr.push(item);
         });
         return db.query('delete from tournament where id in (?)', [delarr], callback);
